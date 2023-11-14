fa523281dca08b4f2fad4b13a9202f7bf16822d2
Yedu Krishna <98282751+YeduKrishnaP@users.noreply.github.com>
2023-11-14 15:51:52 +0530

    Update GUI for Changing Paths.py
    Added option to specify no. of dates to check
    Added some more exception handler blocks

--- GUI for Changing Paths.py
+++ GUI for Changing Paths.py
@@ -5,6 +5,7 @@ sg.theme('DarkAmber')
 
 SRC = ""
 DEST = ""
+CHKDATES = 1
 
 if os.path.exists("Paths.bin"):         # Get the previously saved paths to show
     with open("Paths.bin", "r") as f:
@@ -12,16 +13,16 @@ if os.path.exists("Paths.bin"):         # Get the previously saved paths to show
         SRC = prev_paths[0].strip()
         DEST = prev_paths[1].strip()
 
-
 layout = [  [sg.Text('Change the Paths')],
             [sg.Text('Source Folder:       '), sg.InputText(SRC, size=(200,1))],
             [sg.Text('Destination Folder: '), sg.InputText(DEST, size=(200,1))],
+            [sg.Text('No. of days to check for consistency: '), sg.InputText("1")],
             [sg.Button('Ok'), sg.Button('Close')]
          ]
 
 # Create the Window
 window = sg.Window('Automated Data Transfer', layout,
-                   grab_anywhere = True, size = (600, 125))
+                   grab_anywhere = True, size = (600, 150))
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
@@ -31,20 +32,35 @@ while True:
     
     SRC = values[0]
     DEST = values[1]
+    CHKDATES = values[2]
     
-    if not os.path.exists(SRC): # sanitize
-        sg.popup("Source folder doesn't exist!")
+    try:
+        files = os.listdir(SRC)
+    except FileNotFoundError:
+        sg.popup("Unable to find Source folder or doesn't exist!", title = "Error", modal = True)
+        continue
 
-    elif not os.path.exists(DEST):
-        sg.popup("Destination folder doesn't exist!")
+    if not os.path.exists(DEST):
+        sg.popup("Unable to find Destination folder or doesn't exist!", title = "Error", modal = True)
+        
+    elif SRC == DEST:
+        sg.popup("Source and Destination are the same folder!", title = "Error")
+
+    elif int(CHKDATES) <= 0:
+        sg.popup("Invalid entry for no. of days!", title = "Error", modal = True)
+    
+    elif len(files) < int(CHKDATES):
+        sg.popup("Entered no. of days is more than dates \nin source folder! Try again!", title = "Error", modal = True)
 
     else:
         with open("Paths.bin", "w") as f:
             f.write(SRC)
             f.write("\n")
             f.write(DEST)
+            f.write("\n")
+            f.write(CHKDATES)
             f.close()
 
-        sg.popup("Successfully updated the paths!")
+        sg.popup("Successfully updated the paths!", title = "Success")
         
 window.close()
