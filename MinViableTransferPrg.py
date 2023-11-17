5010585917fb863b1728d19f4aaa714e7c0d0987
Yedu Krishna <98282751+YeduKrishnaP@users.noreply.github.com>
2023-11-17 14:35:37 +0530

    Update MinViableTransferPrg.py

--- MinViableTransferPrg.py
+++ MinViableTransferPrg.py
@@ -7,21 +7,33 @@ import logging as log
 #Init the variables etc.
 
 log.basicConfig(filename = f"./Logfiles/{date.today()}.log", level=log.DEBUG)
+log.info("--------------------------------------------------------------------------------------------------------------------------------------------------------")
+log.info("--------------------------------------------------------------------------------------------------------------------------------------------------------")
+log.info("--------------------------------------------------------------------------------------------------------------------------------------------------------\n\n\n\n\n\n\n")
+log.info("Starting Program")
 
-with open("Paths.bin", "r") as f:
-    PATHS = f.readlines()
+try:
+    with open("Paths.bin", "r") as f:
+        PATHS = f.readlines()
+except:
+    log.error("Unable to open Paths.bin")
+    exit(1)
 
-SOURCE = PATHS[0].replace("\\", "/").strip()    # Read existing paths in the file
-TARGET = PATHS[1].replace("\\", "/").strip()
-CHKDAYS = int(PATHS[2].strip())                 # Read the no. of days to check for
-
+try:
+    SOURCE = PATHS[0].replace("\\", "/").strip()    # Read existing paths in the file
+    TARGET = PATHS[1].replace("\\", "/").strip()
+    CHKDAYS = int(PATHS[2].strip())                 # Read the no. of days to check for
+    
+except:
+    log.error("Error in reading the data from Paths.bin")
+    exit(2)
+    
 DATE_CHK_LIST = list()
-
 for i in range(1, CHKDAYS+1):
     p = str(date.today() - timedelta(days = i)) # convert the dates to required format
     DATE_CHK_LIST.append(p[5:7] + p[8:] + p[:4])# (mmddyyyy) - format in which IHMS stores files
 
-log.debug(DATE_CHK_LIST)
+log.info(f"Dates to check: {DATE_CHK_LIST}")
     
 #-----------------------------------------------------------------------------
 
@@ -34,30 +46,30 @@ def compare_transfer_recursively(src, target): #(tested, working)
          os.walk       : used here to get the subdirs in dir
 
     Then call the function to on each subdir
-
     """
 
     log.debug(f"\tCurrent SRC: {src} | Tar: {target}")
     
     if not os.path.exists(target):  # If target folder doesnt exist, create
-        #log.debug(f"Creating {target}")
         os.mkdir(target)
 
     src_files = os.listdir(src)
     tar_files = os.listdir(target)
 
-    #log.debug(f"Files in src: {src_files} | target: {tar_files}")
     
     for f in src_files: # iterate through files in SRC 
         if f not in tar_files and os.path.isfile(src + f): # Chk if the file is not in TAR
-            shutil.copy2(src + f, target + f)
-            log.debug(f"\t\t\tCopying {src + f} to {target + f}")
+            try:
+                shutil.copy2(src + f, target + f)
+                log.debug(f"\t\t\tCopying {src + f} to {target + f}")
+            except:
+                log.error("Failed to copy file: {f}\n")
 
     subdirs = next(os.walk(src))[1] #gives a list of immediate subdirectories
     for sub in subdirs:
-        log.debug(f"\t\t\tCalling recursion on {src + sub}\n\n")
+        log.debug(f"\t\t\t\n\nCalling recursion on {src + sub}\n\n")
         compare_transfer_recursively(src + sub + "/", target + sub + "/")
-
+        
 #-----------------------------------------------------------------------------
 # Call the function for folders in DATE_CHK_LIST
 
@@ -66,3 +78,5 @@ for date in DATE_CHK_LIST:
     TAR_DATE = TARGET + "/" + date + "/"
     log.debug(f"\n\n\n\n\nCalling Function on date: {SRC_DATE}")
     compare_transfer_recursively(SRC_DATE, TAR_DATE)
+
+log.info("\n\n\n\n\nCompleted Transfer Successfully!\n\n\n\n\n\n\n\n")
