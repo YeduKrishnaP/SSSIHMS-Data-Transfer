336599e0e7da82af0b5fe79c48e723c178407a9d
Yedu Krishna <98282751+YeduKrishnaP@users.noreply.github.com>
2023-11-19 14:22:33 +0530

    Update GUI for Changing Paths.py
    Added Field to take Email IDs for sending daily updates and logs

--- GUI for Changing Paths.py
+++ GUI for Changing Paths.py
@@ -1,66 +1,95 @@
 import PySimpleGUI as sg
 import os
 
-sg.theme('DarkAmber')
+def INIT_PATHS_FILE():
+    """
+    Function that creates a file to store the paths of the Source and Destination
+    folders for the Automated Backup Program to read
 
-SRC = ""
-DEST = ""
-CHKDATES = 1
-
-if os.path.exists("Paths.bin"):         # Get the previously saved paths to show
-    with open("Paths.bin", "r") as f:
-        prev_paths = f.readlines()            
-        SRC = prev_paths[0].strip()
-        DEST = prev_paths[1].strip()
-
-layout = [  [sg.Text('Change the Paths')],
-            [sg.Text('Source Folder:       '), sg.InputText(SRC, size=(200,1))],
-            [sg.Text('Destination Folder: '), sg.InputText(DEST, size=(200,1))],
-            [sg.Text('No. of days to check for consistency: '), sg.InputText("1")],
-            [sg.Button('Ok'), sg.Button('Close')]
-         ]
-
-# Create the Window
-window = sg.Window('Automated Data Transfer', layout,
-                   grab_anywhere = True, size = (600, 150))
-
-# Event Loop to process "events" and get the "values" of the inputs
-while True:
-    event, values = window.read()
-    if event in(sg.WIN_CLOSED, 'Close'): # if user closes window or clicks close
-        break
-    
-    SRC = values[0]
-    DEST = values[1]
-    CHKDATES = values[2]
-    
-    try:
-        files = os.listdir(SRC)
-    except FileNotFoundError:
-        sg.popup("Unable to find Source folder or doesn't exist!", title = "Error", modal = True)
-        continue
-
-    if not os.path.exists(DEST):
-        sg.popup("Unable to find Destination folder or doesn't exist!", title = "Error", modal = True)
         
-    elif SRC == DEST:
-        sg.popup("Source and Destination are the same folder!", title = "Error")
-
-    elif int(CHKDATES) <= 0:
-        sg.popup("Invalid entry for no. of days!", title = "Error", modal = True)
     
-    elif len(files) < int(CHKDATES):
-        sg.popup("Entered no. of days is more than dates \nin source folder! Try again!", title = "Error", modal = True)
+    """    
+    sg.theme('DarkAmber')
 
-    else:
-        with open("Paths.bin", "w") as f:
-            f.write(SRC)
-            f.write("\n")
-            f.write(DEST)
-            f.write("\n")
-            f.write(CHKDATES)
-            f.close()
+    SRC = ""
+    DEST = ""
+    datesprompt = "(eg: 3 means the program checks 3 previous days for consistency)"
+    mailprompt = "Use commas to add multiple Email IDs (eg: xyz@abc.com,abc@xyz.com)"
+    
+    
+    if os.path.exists("Paths.bin"):         # Get the previously saved paths to show
+        with open("Paths.bin", "r") as f:
+            prev_paths = f.readlines()            
+            SRC = prev_paths[0].strip()
+            DEST = prev_paths[1].strip()
 
-        sg.popup("Successfully updated the paths!", title = "Success")
+
+    layout = [  [sg.Text('Change the Paths')],
+                [sg.Text('Source Folder:       '), sg.InputText(SRC, size=(200,1))],
+                [sg.Text('Destination Folder: '), sg.InputText(DEST, size=(200,1))],
+                [sg.Text('No. of days to check for consistency: '), sg.InputText(datesprompt, size = (700, 1))],
+                [sg.Text('Enter your email to receive notifications: '), sg.InputText(mailprompt, size = (700, 1))],
+                [sg.Button('Ok'), sg.Button('Close')]
+             ]
+
+    # Create the Window
+    window = sg.Window('Automated Data Transfer', layout,
+                       grab_anywhere = True, size = (800, 175))
+
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
+        if event in(sg.WIN_CLOSED, 'Close'): # if user closes window or clicks close
+            window.close()
+            return 0
         
-window.close()
+        SRC = values[0]
+        DEST = values[1]
+        CHKDATES = values[2]
+        MAIL_LIST = values[3]
+
+        try:
+            CHKDATES = int(CHKDATES)
+        except ValueError:
+            sg.popup("Invalid entry for no. of days! Enter a number!", title = "Error", modal = True)
+            continue
+
+        if MAIL_LIST == mailprompt:
+            sg.popup("Kindly enter the mail id for receiving notifications", title = "Mail Error", modal = True)
+            continue
+        
+        
+        try:
+            files = os.listdir(SRC)
+        except FileNotFoundError:
+            sg.popup("Unable to find Source folder or doesn't exist!", title = "Error", modal = True)
+            continue
+
+        if not os.path.exists(DEST):
+            sg.popup("Unable to find Destination folder or doesn't exist!", title = "Error", modal = True)
+            
+        elif SRC == DEST:
+            sg.popup("Source and Destination are the same folder!", title = "Error")
+
+        elif int(CHKDATES) <= 0:
+            sg.popup("Invalid entry for no. of days!", title = "Error", modal = True)
+        
+        elif len(files) < int(CHKDATES):
+            sg.popup("Entered no. of days is more than dates \nin source folder! Try again!", title = "Error", modal = True)
+
+        else:
+            with open("Paths.bin", "w") as f:
+                f.write(SRC)
+                f.write("\n")
+                f.write(DEST)
+                f.write("\n")
+                f.write(str(CHKDATES))
+                f.write("\n")
+                f.write(MAIL_LIST)
+                f.close()
+
+            sg.popup("Successfully updated the paths!", title = "Success")
+    window.close()
+
+if __name__ == "__main__":
+    INIT_PATHS_FILE()
