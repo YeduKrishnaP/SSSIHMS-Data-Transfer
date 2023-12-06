3df70dc1572f5df945747a9e523998b70fa5fae5
Yedu Krishna <98282751+YeduKrishnaP@users.noreply.github.com>
2023-12-06 14:42:20 +0530

    Update EditTransferPaths.py
    Added the Create_Task() function that uses Pywin32 library to schedule the required task

--- EditTransferPaths.py
+++ EditTransferPaths.py
@@ -1,13 +1,62 @@
 import PySimpleGUI as sg
 import os
+import subprocess
+import win32com.client
+
+def Create_Task(user_password):
+
+    cur_user = os.getlogin()
+    computer_name = os.environ['COMPUTERNAME']
+    cur_dir = os.getcwd()
+    sch_file = cur_dir + "\\" + "ScheduleThis.bat"
+
+    SCH = win32com.client.Dispatch('Schedule.Service')
+    SCH.Connect()
+    root = SCH.GetFolder('\\')
+
+    TASK = SCH.NewTask(0)
+
+    #Define Trigger properties
+    TriggerType = 2 #Daily
+    trigger = TASK.Triggers.Create(TriggerType)
+    starttime = "2023-12-04T14:30:00"   #yyyy-mm-ddThh-mm-ss
+    trigger.StartBoundary = starttime
+    trigger.DaysInterval = 1
+    trigger.Enabled = True
+    trigger.Id = "DailyTrigger"
+
+    #Define Actions of task
+    action = TASK.Actions.Create(0) # 0 refers to the type execute
+    action.Path = "cmd.exe"
+    action.Arguments = f'/c start "" {sch_file}'
+    action.ID = "ADTTestPywin32"
+    action.WorkingDirectory = cur_dir
+
+    #Addtnl task info
+    TASK.RegistrationInfo.Description = "Testing ADT scheduling using pywin32"
+    TASK.Settings.Enabled = True
+    TASK.Settings.StopIfGoingOnBatteries = False
+    task_create_or_update = 6
+    task_logon_none = 1
+    TASK.Principal.UserId = cur_user
+    TASK.Principal.LogonType = 6
+
+    root.RegisterTaskDefinition(
+        "ADTTestPywin32",
+        TASK,
+        task_create_or_update,
+        f"{computer_name}\\{cur_user}",
+        user_password,
+        task_logon_none
+        )
+
 
 def INIT_PATHS_FILE():
     """
     Function that creates a file to store the paths of the Source and Destination
     folders for the Automated Backup Program to read
 
-        
-    
+    Libraries used: PySimpleGUI, OS
     """    
     sg.theme('Topanga')
 
@@ -29,7 +78,8 @@ def INIT_PATHS_FILE():
                 [sg.Text('Destination Folder: '), sg.InputText(DEST), sg.FolderBrowse()],
                 [sg.Text('No. of days to check for consistency: '), sg.InputText(datesprompt, size = (600, 1))],
                 [sg.Text('Enter your email to receive notifications: '), sg.InputText(mailprompt, size = (675, 1))],
-                [sg.Button('Ok'), sg.Button('Close')]
+                [sg.Text('Enter the password for the current user: '), sg.InputText("Your Password is not stored anywhere by this program")],
+                [sg.Button('Ok'), sg.Button('Transfer Now'), sg.Button('Close')]
              ]
 
     # Create the Window
@@ -42,27 +92,25 @@ def INIT_PATHS_FILE():
         if event in(sg.WIN_CLOSED, 'Close'): # if user closes window or clicks close
             window.close()
             return 0
-        
+
         SRC = values[0]
         DEST = values[1]
         CHKDATES = values[2]
         MAIL_LIST = values[3]
-
+        PASSWORD = values[4]
+         
         try:
+            files = os.listdir(SRC)
             CHKDATES = int(CHKDATES)
+        except FileNotFoundError:
+            sg.popup("Unable to find Source folder or doesn't exist!", title = "Error", modal = True)
+            continue
+
         except ValueError:
             sg.popup("Invalid entry for no. of days! Enter a number!", title = "Error", modal = True)
             continue
 
-        if MAIL_LIST == mailprompt:
-            sg.popup("Kindly enter the mail id for receiving notifications", title = "Mail Error", modal = True)
-            continue
-        
-        
-        try:
-            files = os.listdir(SRC)
-        except FileNotFoundError:
-            sg.popup("Unable to find Source folder or doesn't exist!", title = "Error", modal = True)
+        except:
             continue
 
         if not os.path.exists(DEST):
@@ -76,6 +124,9 @@ def INIT_PATHS_FILE():
         
         elif len(files) < int(CHKDATES):
             sg.popup("Entered no. of days is more than dates \nin source folder! Try again!", title = "Error", modal = True)
+            
+        elif MAIL_LIST == mailprompt or MAIL_LIST == '':
+            sg.popup("Kindly enter the mail id for receiving notifications", title = "Mail Error", modal = True)
 
         else:
             with open("Paths.bin", "w") as f:
@@ -87,9 +138,14 @@ def INIT_PATHS_FILE():
                 f.write("\n")
                 f.write(MAIL_LIST)
                 f.close()
-
-            sg.popup("Successfully updated the paths!", title = "Success")
-    window.close()
+                Create_Task(PASSWORD)
+            
+            if event == 'Transfer Now':
+                sg.popup("Kindly Wait! Transferring files!", title = "Transferring", modal = True)
+                exe_path = os.getcwd() + "\TransferPrg.exe"
+                subprocess.call(exe_path, shell=True)
+            sg.popup("Successfully updated the paths!", title = "Success")    
 
 if __name__ == "__main__":
     INIT_PATHS_FILE()
+
